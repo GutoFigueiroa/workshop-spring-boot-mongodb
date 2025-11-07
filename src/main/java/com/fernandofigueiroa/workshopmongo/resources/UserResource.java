9c77ce72083c7d03cdc4f937d3f0f916a4431484
Guto Figueiroa <fernando.figueiroa.pe@gmail.com>
2025-11-07 11:52:56 -0300

    Endpoint para retornar os posts de um usuario

--- src/main/java/com/fernandofigueiroa/workshopmongo/resources/UserResource.java
+++ src/main/java/com/fernandofigueiroa/workshopmongo/resources/UserResource.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.fernandofigueiroa.workshopmongo.domain.Post;
 import com.fernandofigueiroa.workshopmongo.domain.User;
 import com.fernandofigueiroa.workshopmongo.dto.UserDTO;
 import com.fernandofigueiroa.workshopmongo.services.UserService;
@@ -59,5 +60,13 @@ public class UserResource {
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 	}
+	
+	@RequestMapping(value = "/{id}/posts", method = RequestMethod.GET) // passar parametros na url, Q(consulta) status 200(ok)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
+	}
+	
+	
 
 }

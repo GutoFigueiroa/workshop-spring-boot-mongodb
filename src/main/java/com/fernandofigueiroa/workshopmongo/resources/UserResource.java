3daaabe30c0d3cb36bfae47254f3b1bc42eda014
Guto Figueiroa <fernando.figueiroa.pe@gmail.com>
2025-11-07 10:35:32 -0300

    Criando Entity POST com user aninhado

--- src/main/java/com/fernandofigueiroa/workshopmongo/resources/UserResource.java
+++ src/main/java/com/fernandofigueiroa/workshopmongo/resources/UserResource.java
@@ -13,54 +13,47 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import com.fernandofigueiroa.workshopmongo.config.Instantiation;
 import com.fernandofigueiroa.workshopmongo.domain.User;
 import com.fernandofigueiroa.workshopmongo.dto.UserDTO;
 import com.fernandofigueiroa.workshopmongo.services.UserService;
 
-@RestController
-@RequestMapping(value= "/users")
+@RestController // Controller é a entrada do sistema.
+@RequestMapping(value = "/users")
 public class UserResource {
 
-    private final Instantiation instantiation;
-	
-	@Autowired
+	@Autowired // Instancia a classe caso não exista, isso é injeção de dependecia
 	private UserService service;
 
-    UserResource(Instantiation instantiation) {
-        this.instantiation = instantiation;
-    }
-	
 	@GetMapping
-	public ResponseEntity<List<UserDTO>> findAll(){
+	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
 	}
-	
-	@RequestMapping(value="/{id}", method=RequestMethod.GET)
-	public ResponseEntity<UserDTO> findById(@PathVariable String id){
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET) // passar parametros na url, Q(consulta) status 200(ok)
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
-	
-	@RequestMapping(method=RequestMethod.POST)
-	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto){
+
+	@RequestMapping(method = RequestMethod.POST) // Encapsula dados, formulários e informações são empacotados no
+													// cabeçalho
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
 		User obj = service.fromDTO(objDto);
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
-	
-	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
-	public ResponseEntity<Void> delete(@PathVariable String id){
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable String id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
-	
-	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
-	public ResponseEntity<Void> updatae(@RequestBody UserDTO objDto, @PathVariable String id){
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> updatae(@RequestBody UserDTO objDto, @PathVariable String id) {
 		User obj = service.fromDTO(objDto);
 		obj.setId(id);
 		obj = service.update(obj);
